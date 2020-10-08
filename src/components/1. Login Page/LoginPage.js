65eb6f3961a6705b174630f3c242afa0a7e4947d
unknown <glhrmpaiva@gmail.com>
2020-10-08 17:34:39 -0300

    Realizada integração com o backend nos endpoints de login e signup

--- src/components/1. Login Page/LoginPage.js
+++ src/components/1. Login Page/LoginPage.js
@@ -5,21 +5,53 @@ import {useHistory} from 'react-router-dom';
 const LoginPage = () => {
   const history = useHistory()
 
-  const baseUrl = "http://localhost: 3000"
-
-  const goToProfilePage = () => {
-    history.push("/profile")
-  }
+  const baseUrl = "https://flickenu.herokuapp.com"
 
   const goToSignupPage = () => {
     history.push("/signup")
   }
 
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const goToProfilePage = () => {
+    const body = {
+      email: email,
+      username: username,
+      password: password
+    }
+    axios.post(`${baseUrl}/user/login`, body)
+    .then((response) => {
+      window.localStorage.setItem("token", response.data.token)
+      history.push("/profile")
+    }).catch((error) => {
+      alert("Dados incorretos, por favor tente novamente")
+      console.log(error.message)
+      setEmail("")
+      setUsername("")
+      setPassword("")
+    }) 
+  }
+
+  const onChangeEmail = event => {
+    setEmail(event.target.value)
+  }
+
+  const onChangeUsername = event => {
+    setUsername(event.target.value)
+  }
+
+  const onChangePassword = event => {
+    setPassword(event.target.value)
+  }
+
   return (
     <div>
-      <h>LOGIN PAGE</h>
-      <input placeholder="Nome de Usuário ou Email" />
-      <input placeholder="Senha" />
+      <p>LOGIN PAGE</p>
+      <input placeholder="Nome de Usuário" value={username} onChange={onChangeUsername} />
+      <input placeholder="Email" value={email} onChange={onChangeEmail} />
+      <input placeholder="Senha" onChange={onChangePassword} type="password" />
       <button onClick={goToProfilePage}>Logar</button>
       <button onClick={goToSignupPage}>Cadastre-se</button>
     </div>
--- src/components/2. Signup Page/SignupPage.js
+++ src/components/2. Signup Page/SignupPage.js
@@ -5,7 +5,7 @@ import {useHistory} from 'react-router-dom';
 const SignupPage = () => {
   const history = useHistory()
 
-  const baseUrl = "http://localhost: 3000"
+  const baseUrl = "https://flickenu.herokuapp.com"
 
   const goToProfilePage = () => {
     history.push("/profile")
@@ -15,14 +15,65 @@ const SignupPage = () => {
     history.push("/login")
   }
 
+  const [username, setUsername] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [role, setRole] = useState("")
+
+  const signup = () => {
+    const body = {
+      username: username,
+      name: name,
+      email: email,
+      password: password,
+      role: role
+    }
+    axios.post(`${baseUrl}/user/signup`, body)
+    .then((response) => {
+      alert("Usuário criado com sucesso, redirecionando agora para seu perfil!")
+      window.localStorage.setItem("token", response.data.token)
+      history.push("/profile")
+    }).catch((error) => {
+      alert("Erro ao criar usuário, por favor tente novamente.")
+      console.log(error.message)
+    })
+  }
+
+  const onChangeUsername = event => {
+    setUsername(event.target.value)
+  }
+
+  const onChangeName = event => {
+    setName(event.target.value)
+  }
+
+  const onChangeEmail = event => {
+    setEmail(event.target.value)
+  }
+
+  const onChangePassword = event => {
+    setPassword(event.target.value)
+  }
+
+  const onChangeRole = event => {
+    setRole(event.target.value)
+  }
+
   return (
     <div>
-      <h>SIGNUP PAGE</h>
-      <input placeholder="Nome de Usuário" />
-      <input placeholder="Nome completo" />
-      <input placeholder="Email" />
-      <input placeholder="Senha" />
-      <button onClick={goToProfilePage}>Cadastrar</button>
+      <p>SIGNUP PAGE</p>
+      <input placeholder="Nome de Usuário" value={username} onChange={onChangeUsername} />
+      <input placeholder="Nome completo" value={name} onChange={onChangeName} />
+      <input placeholder="Email" value={email} onChange={onChangeEmail} />
+      <input placeholder="Senha" value={password} onChange={onChangePassword} type="password" />
+      <input placeholder="Confirme sua senha" type="password" />
+      <select value={role} onChange={onChangeRole}>
+        <option>Selecione seu cargo</option>
+        <option>NORMAL</option>
+        <option>ADMIN</option>
+      </select>
+      <button onClick={signup}>Cadastrar</button>
       <button onClick={goToLoginPage}>Já é cadastrado? Volte e faça login</button>
     </div>
   )
